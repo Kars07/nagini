d2ac0d40cc63fa9a448f1bfc50dc827a843899a0
Kars07 <eniaiyejuni.kayode@gmail.com>
2024-10-21 22:24:40 +0100

    Add files via upload
    This is a code used to decrypt an encrypted code with an unknown key value.

---
+++ module2.py
@@ -0,0 +1,28 @@
+import string
+def decrypt(encrypted_code, key_value):
+    key_code = string.ascii_uppercase
+    decrypt_positon = []
+    decrypt_plain_text = []
+    
+    for char in encrypted_code:
+        keycode_position = key_code.index(char.upper())
+        
+        new_keycode_position = keycode_position - key_value
+
+        if new_keycode_position < 0:
+            new_keycode_position += 26
+            
+        decrypt_positon.append(new_keycode_position)
+        decrypt_plain_text.append(key_code[new_keycode_position])
+    
+    return decrypt_positon, decrypt_plain_text
+
+encrypted_code = input("INPUT ENCRYPTED CODE: ").upper()
+if all(char in string.ascii_uppercase for char in encrypted_code):
+     for key_value in range(0, 25):
+      decrypted_position, decrypted_plain_text = decrypt(encrypted_code, key_value)
+      print(f"Encrypted Code: {encrypted_code}")
+      print(f"Decrypted Position after key value({key_value}): {decrypted_position}")
+      print(f"Decrypted to Plain text: {decrypted_plain_text}") 
+else:
+    print("Invalid Encrypted Code")    
